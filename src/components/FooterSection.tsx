cd107f53f85af8ce0a0f6cf570c156c626c7a330
TEO <alfredo.ssm@gmail.com>
2025-05-03 18:28:53 -0400

    se agrega github icon y redirect a portafolio

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,4 +1,4 @@
-import { Instagram, MessageCircle, Twitter } from "lucide-react";
+import { Github, Instagram, MessageCircle, Twitter } from "lucide-react";
 const logo = "/TCG-Market-logo.png";
 
 export const FooterSection: React.FC = () => {
@@ -10,14 +10,17 @@ export const FooterSection: React.FC = () => {
 
       {/* Íconos de redes sociales con Lucide */}
       <div className="flex justify-center space-x-6 mt-6 text-2xl">
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">
+        <a href="https://instagram.com/teoisnotdead" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">
           <Instagram size={24} />
         </a>
         <a href="https://wa.me/123456789" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">
           <MessageCircle size={24} />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">
+        <a href="https://twitter.com/teoisnotdead" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">
           <Twitter size={24} />
+        </a>
+        <a href="https://github.com/teoisnotdead" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">
+          <Github size={24} />
         </a>
       </div>
 
@@ -25,7 +28,7 @@ export const FooterSection: React.FC = () => {
       <p className="mt-6 text-xs text-gray-400 uppercase">
         <span className="text-[#F19F00]"></span>
         © 2025 <span className="text-[#F19F00]">TCG MARKET</span>.
-        Desarrollado por <span className="text-[#F19F00]">Alfredo Saavedra </span>
+        Desarrollado por <span className="text-[#F19F00]"><a href="https://teodev.cl" target="_blank" rel="noopener noreferrer" className="hover:text-[#F19F00] transition-all">Alfredo Saavedra</a> </span>
         para <span className="text-[#F19F00]">Desafío Latam</span>.
       </p>
     </footer>
